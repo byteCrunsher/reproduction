8af929f005b0e579fa77390c81b7ad2e36bb26c8
byteCrunsher <54635322+byteCrunsher@users.noreply.github.com>
2019-10-28 10:02:19 +0100

    Update router.js

--- apps/app-one/src/router.js
+++ apps/app-one/src/router.js
@@ -7,45 +7,46 @@ import Two from './views/Two.vue';
 Vue.use(Router);
 
 const router = new Router({
-    base: '/app-one/',
-    mode: 'history',
-    routes: [{
-            path: '/',
-            component: One,
-        },
-        {
-            path: '/two',
-            component: Two,
-        },
-    ],
+	base: '/app-one/',
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			component: One,
+		},
+		{
+			path: '/two',
+			component: Two,
+		},
+	],
 });
 
 if (window.location !== window.parent.location) {
-    const browserUrl = top.location.href;
-    history.replaceState(null, null, browserUrl);
+	const browserUrl = top.location.href;
+	history.replaceState(null, null, browserUrl);
 }
 
 router.afterEach((to, from) => {
-    let newPath = '/' + to.path.split('/').pop();
-    const matchingRoutes = router.options.routes.filter(
-        route => route.path === newPath
-    );
-    const isPathInRoutes = matchingRoutes.length > 0;
+	let newPath = '/' + to.path.split('/').pop();
+	const matchingRoutes = router.options.routes.filter(
+		route => route.path === newPath
+	);
+	const isPathInRoutes = matchingRoutes.length > 0;
 
-    if (newPath === router.history.base || !isPathInRoutes) {
-        newPath = '/';
-    }
+	if (newPath === router.history.base || !isPathInRoutes) {
+		newPath = '/';
+	}
 
-    let storagePath;
+	let storagePath;
 
-    if (router.currentRoute.path !== newPath) {
-        router.push(newPath);
-        storagePath = newPath;
-    } else {
-        storagePath = to.path;
-    }
+	if (router.currentRoute.path !== newPath) {
+		router.push(newPath);
+		storagePath = newPath;
+	} else {
+		storagePath = to.path;
+	}
 
-    localStorage.subAppRouteUpdate = storagePath;
+	localStorage.subAppRouteUpdate = storagePath;
 });
 
 export default router;
